f373aa91bccd5a04a573c7e6aaaac9e39aaa7f8a
Jake Gregg <greggjs@miamioh.edu>
2014-01-22 13:54:56 -0500

    Initial Commit

---
+++ DeckManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DeckManager
+//
+//  Created by Jake Gregg on 1/22/14.
+//  Copyright (c) 2014 Rememerme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
